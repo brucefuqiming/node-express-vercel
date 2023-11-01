4809d1b472e22207fca65497b52b8116d98f0f01
付啟明 <brucefuqiming@gmail.com>
2023-11-01 11:45:09 +0800

    change

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 router.get("/", async (req, res, next) => {
   return res.status(200).json({
-    title: "Express Testing",
+    title: "Express Testing2",
     message: "The app is working properly!",
   });
 });
